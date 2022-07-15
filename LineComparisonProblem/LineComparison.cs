4e0214c426228935202fc134c20f4af2e7b0a2aa
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-16 02:30:04 +0530

    [Sushree | Refactored extracted length of line function to a method]

--- LineComparisonProblem/LineComparison.cs
+++ LineComparisonProblem/LineComparison.cs
@@ -8,20 +8,8 @@ namespace LineComparisonProblem
 {
     class LineComparison
     {
-        static void Main(string[] args)
+        static double lengthOfLine(int x1, int x2, int y1,int y2)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program\n");
-
-            Console.WriteLine("Enter the cordinates of first points");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The cordicates of first point is ({0},{1})" , x1, y1);
-
-            Console.WriteLine("Enter the cordinates of second points");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The cordicates of second point is ({0},{1})", x2, y2);
-
             int diffOfXCordinate = x2 - x1;
             int diffOfYCordinate = y2 - y1;
 
@@ -29,18 +17,26 @@ namespace LineComparisonProblem
             double YExp2 = Math.Pow(diffOfYCordinate, 2);
 
             double lengthOfLine = Math.Sqrt(XExp2 + YExp2);
-            Console.WriteLine("Length of the line is " + lengthOfLine + " CM");
+            return lengthOfLine;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program\n");
+          
+            Console.WriteLine("Enter the poitns of first cordinator");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The cordicates of first point is ({0},{1})" , x1, y1);
 
-            bool checkLines = diffOfXCordinate.Equals(diffOfYCordinate);
-            if (checkLines)
-            {
-                Console.WriteLine("Lines are equal.");
-            }
-            else
-            {
-                Console.WriteLine("Lines are not equal.");
-            }
+            Console.WriteLine("Enter the points of second cordinator");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The cordicates of second point is ({0},{1})", x2, y2);
 
+            double lenghtOfLines = lengthOfLine(x1, x2, y1, y2);
+            
+            Console.WriteLine("Length of the line is " + lenghtOfLines + " CM");
 
             Console.ReadLine();
 
